4e6bf9ce5c669286f6c6836e517ee156ed9abd26
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2022-03-05 22:52:05 +0530

    Added ogv signup template

---
+++ volunteer.php
@@ -0,0 +1,12 @@
+<?php
+
+$background = "background-ogv.png";
+$logo = "ogv.png";
+$product = "oGTa";
+$color = "#F85A40";
+$product_name = "Global Volunteer";
+$product_name_up = "GLOBAL VOLUNTEER";
+
+include_once("template1.php");
+
+?>
