a268d2d6f6b85fba55688341d05faee201a76106
kaneeldias <kaneeldias@hotmail.com>
2022-04-25 19:08:10 +0530

    Rever changes to base

--- base.php
+++ base.php
@@ -1,8 +1,8 @@
 <?php
 
-$base = "http://localhost:80/aiesec.lk/apply/";
+$base = "http://localhost:80/apply.aiesec.lk/";
 if ($_SERVER['SERVER_NAME'] == "localhost"){
-    $base = "https://localhost/aiesec.lk/apply/";
+    $base = "https://localhost/apply.aiesec.lk/";
 }
 
 ?>
